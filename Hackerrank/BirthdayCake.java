72acf9ddedac6d0e1b6f2ed095a5b70cf8642db1
5AbhishekSaxena <5abhisheksaxena@gmail.com>
2018-10-19 15:37:55 +0530

    Create BirthdayCake.java

---
+++ Hackerrank/BirthdayCake.java
@@ -0,0 +1,30 @@
+/**
+* https://www.hackerrank.com/challenges/birthday-cake-candles/copy-from/64241890
+**/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class BirthdayCake {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        int max = 0;
+        for(int i = 0; i < n; i++){
+            arr[i] = in.nextInt();
+            if(max<arr[i])
+                max = arr[i];
+        }
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            if(max == arr[i])
+                 count++;
+        }   
+            
+        System.out.println(count);
+    }
+}
