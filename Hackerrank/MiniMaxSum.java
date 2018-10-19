b43640217036cc6786b900f32e6b3d9048d5b1bf
5AbhishekSaxena <5abhisheksaxena@gmail.com>
2018-10-19 15:42:31 +0530

    Create MiniMaxSum.java

---
+++ Hackerrank/MiniMaxSum.java
@@ -0,0 +1,42 @@
+/**
+* https://www.hackerrank.com/challenges/mini-max-sum/copy-from/64241630
+**/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class MiniMaxSum {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[5];
+        
+        
+        for(int i = 0; i < 5;   i++)
+            arr[i] = in.nextInt();
+        int min=arr[0],max=0;
+        for(int i=0;i<5;i++){
+                if(arr[i]>max)
+                   max = arr[i];
+            if(arr[i]<min)
+                    min = arr[i];
+        }
+        long maxSum = 0, minSum = 0;
+        for(int i=0;i<5;i++){
+            if(min == max && i!=4){
+                maxSum +=arr[i];
+                minSum +=arr[i];
+            }
+            if(arr[i] != min)
+                maxSum += arr[i];
+            if(arr[i] !=max)
+                minSum +=arr[i];
+        }
+        
+        System.out.print(minSum + " " + maxSum);
+        in.close();
+    }
+}
