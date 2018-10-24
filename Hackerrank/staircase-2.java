76260ad4147f4ef743ff2b3459d266138e0072ef
Owais Ali <owaisali.cs@gmail.com>
2018-10-25 00:25:24 +0500

    code of staircase.java

--- Hackerrank/staircase-2.java
+++ Hackerrank/staircase-2.java
@@ -1,25 +1,27 @@
-/**
-* https://www.hackerrank.com/challenges/staircase/copy-from/70623634
-**/
-
 import java.io.*;
-import java.util.*;
-import java.text.*;
 import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
 import java.util.regex.*;
-
-public class Staircase {
-
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-         for(int k=n;k>0;k--){
-	         for(int i=1;i<k;i++)
-		        System.out.print(" ");
-		     for(int j=0;j<n-k+1;j++)
-               System.out.print("#");
-            System.out.print("\n");
-         }
-        in.close();
+ public class Solution {
+     // Complete the staircase function below.
+    static void staircase(int n) {
+        int m=n;
+        
+        while(n!=0){
+            n--;
+            for(int i=0;i<n;i++)   System.out.print(" ");
+            for(int i=0;i<m-n;i++) System.out.print("#");
+            System.out.println();
+        }
+    }
+     private static final Scanner scanner = new Scanner(System.in);
+     public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+         staircase(n);
+         scanner.close();
     }
 }
