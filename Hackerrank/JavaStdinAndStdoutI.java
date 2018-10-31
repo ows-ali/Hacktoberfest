15c5937d9f691e970757c80258054c679e8bef6f
dollyd <dollyd@gmail.com>
2018-10-30 23:03:53 -0400

    Solution for Java Stdin and Stdout 1 problem

---
+++ Hackerrank/JavaStdinAndStdoutI.java
@@ -0,0 +1,19 @@
+/*
+* https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem
+*/
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        scan.close();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
