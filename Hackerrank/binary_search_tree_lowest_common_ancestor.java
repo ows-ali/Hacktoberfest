deb9caf0876efa3b8fbc6ac43a70fe263435dc51
kotlasaicharanreddy <kotlasaicharan@yahoo.com>
2019-10-01 22:42:15 +0530

    added LCA

---
+++ Hackerrank/binary_search_tree_lowest_common_ancestor.java
@@ -0,0 +1,75 @@
+/*
+https://www.hackerrank.com/challenges/binary-search-tree-lowest-common-ancestor/problem 
+*/
+
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+
+	/*
+    class Node 
+    	int data;
+    	Node left;
+    	Node right;
+	*/
+	public static Node lca(Node root, int v1, int v2) {
+      	if(root==null){
+              return null;
+          }
+          if(root.data==v1 || root.data==v2){
+              return root;
+          }
+          Node l = lca(root.left,v1,v2);
+          Node r = lca(root.right,v1,v2);
+          if(l!=null && r!=null){
+              return root;
+          }
+          else{
+              return (l==null?r:l);
+          }
+    }
+
+	public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+      	int v1 = scan.nextInt();
+      	int v2 = scan.nextInt();
+        scan.close();
+        Node ans = lca(root,v1,v2);
+        System.out.println(ans.data);
+    }	
+}
