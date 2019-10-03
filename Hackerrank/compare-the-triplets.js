52291df2c8d34a43c54c9d58e977340592cb2b97
bruno-gois <brunovgois@gmail.com>
2019-10-03 20:37:44 -0300

    add compare the triplets solution to JS

---
+++ Hackerrank/compare-the-triplets.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the compareTriplets function below.
+function compareTriplets(a, b) {
+    let score = [0,0]
+
+    for (let i = 0; i < a.length; i++)
+        a[i] > b[i] ? score[0]++ : a[i] < b[i] ? score[1]++ : ""
+    return score
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const b = readLine().replace(/\s+$/g, '').split(' ').map(bTemp => parseInt(bTemp, 10));
+
+    const result = compareTriplets(a, b);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
