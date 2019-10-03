71bb5d257b464d6dec0ab15b0d21671403bff4ef
Danny Suarez <dsuarezlogans@gmail.com>
2019-10-03 11:42:41 -0500

    add hackerrank solution for sock-merchant problem in js

---
+++ Hackerrank/sock-merchant.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the sockMerchant function below.
+function sockMerchant(n, ar) {
+    const typesCount = {};
+    let pairs = 0;
+    for (const item of ar) {
+        if (typesCount[item]) {
+            typesCount[item] = 0;
+            pairs++;
+        } else {
+            typesCount[item] = 1;
+        }
+    }
+    return pairs
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    let result = sockMerchant(n, ar);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
