a5ebe65e555f307282aaaa8142c56c630aece9de
Anton de Regt <antonderegt@gmail.com>
2019-10-04 16:12:03 +0200

    Add java solution to Hackerrank problem: Java Loops 1

---
+++ Hackerrank/java-loops-1.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        for(int i = 1; i < 11; i++){
+            System.out.println(N + " x " + i + " = " + (N*i));
+        }
+    }
+}
+
+
