bdca629564f94c6809b3b0de3ea33615e0b01b58
Nick Robert <48125267+robo-nic@users.noreply.github.com>
2019-10-04 09:58:43 +0530

    added solution for Codechef/CHEFDIL

--- Codechef/CHEFDIL.cpp
+++ Codechef/CHEFDIL.cpp
@@ -2,54 +2,12 @@
 https://www.codechef.com/AUG19B/problems/CHEFDIL
 */
 
+// AC on all test cases
+
 #include <iostream>
-#include <vector>
+#include <string>
 
 using namespace std;
-
-const long int modulo_value = 1000000007;
-
-unsigned long long int get_10_raise_to_power(int e){
-    unsigned long long int ans{1};
-    if(e == 0)
-        return 1;
-
-    for(int i=0; i<e; i++){
-        ans *= 10;
-    }
-
-    return ans;
-}
-
-vector<int> number_to_vector(long long int x){
-
-    vector<int> ans;
-
-    while(x > 0){
-        ans.push_back(x%10);
-        x /= 10;
-    }
-
-    return ans;
-}
-
-long int f(long long int x){
-
-    int e{0};
-    int last_one{-1};
-
-    long int value{0};
-
-    vector<int> number = number_to_vector(x);
-    number.push_back(-1); // dummy value
-
-    for(int e=0; e<number.size()-1; e++)
-        if(number.at(e) != number.at(e+1))
-            value = (value + number.at(e)*get_10_raise_to_power(e))%modulo_value;
-
-    return value;
-
-}
 
 int main(){
 
@@ -58,23 +16,22 @@ int main(){
 
     while(t--){
 
-        int nl{0};
-        long long int l{0};
-        cin >> nl >> l;
+        string s;
+        cin >> s;
 
-        int nr{0};
-        long long int r{0};
-        cin >> nr >> r;
+        int n = s.size();
 
+        int count_one{0};
 
-        long int password_value{0};
-
-        for(long long int x=l%modulo_value; x<=r%modulo_value; x++){
-            password_value = (password_value+f(x))%modulo_value;
+        for(int i=0; i<n; i++){
+            if((s[i]-'0') == 1)
+                count_one++;
         }
 
-        cout << password_value << endl;
-
+        if(count_one%2 == 0)
+            cout << "LOSE" << endl;
+        else
+            cout << "WIN" << endl;
     }
 
 }
