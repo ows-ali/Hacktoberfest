0fd72af7fd3ab24dfab6a4c1e930d25eed07589b
Julio Dias <julioocdias@gmail.com>
2019-10-05 21:57:25 -0300

    Adding 30 Days of Code - Recursion 3 solution and adding my profile to contributors.

---
+++ Hackerrank/recursion_3.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    static int factorial(int n) {
+        if (n == 0) return 1;
+        return n = (n * factorial(n - 1));
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int result = factorial(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
