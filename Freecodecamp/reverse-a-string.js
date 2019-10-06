96e88233cf9018d656d62177ff27538ea6e9befc
ritu7488 <56237404+ritu7488@users.noreply.github.com>
2019-10-07 04:20:15 +0530

    Update reverse-a-string.js

--- Freecodecamp/reverse-a-string.js
+++ Freecodecamp/reverse-a-string.js
@@ -1,5 +1,6 @@
+//Defining a function for reversing a string
 function reverseString(str) {
   return str.split('').reverse().join('');
 }
-
+//Passing a value into reverse string function
 reverseString("hello");
