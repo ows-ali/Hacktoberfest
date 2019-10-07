a0720b10cb29d6cf52cef08f660ce6bc8e76132e
Sarah Sakhri <sarah.sakhri@gmail.com>
2019-10-06 23:54:52 -0400

    updated readme and rightstaircase

---
+++ Hackerrank/StaircaseRight.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class StaircaseRight{
+
+    static void staircase(int n) {
+
+        for(int i=n; i>0 ; i--){
+
+             for(int j=1; j < i ; j++)
+
+                System.out.print(" ");
+
+             for(int k = 0 ; k < n-i+ 1 ; k++)
+
+               System.out.print("#");
+
+            System.out.print("\n");
+         }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        staircase(n);
+
+        scanner.close();
+    }
+}
