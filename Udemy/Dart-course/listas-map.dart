96ec48f28634d7396260c1ca6676107018de4749
Jennifer Omena <jennifer.omena@m4u.com.br>
2019-10-07 19:00:04 -0300

    dart udemy course added by gennipher

---
+++ Udemy/Dart-course/listas-map.dart
@@ -0,0 +1,12 @@
+void main() {
+  Map usuario = {
+    'nome': 'Jennifer',
+    'idade': 25,
+    'altura': 1.73,
+    'jennifer.dev': true,
+    25: 'vinte e cinco anos',
+  };
+  print(usuario['nome']);
+  print(usuario['idade']);
+  print(usuario[25]);
+}
