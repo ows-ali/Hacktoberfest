5ee08165df131bb21c59e5ab1595f735573229a8
Sarah Sakhri <sarah.sakhri@gmail.com>
2019-10-07 12:04:33 -0400

    very sig sum solution

--- Hackerrank/StaircaseRight.java
+++ Hackerrank/StaircaseRight.java
@@ -6,26 +6,27 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class StaircaseRight{
+public class StaircaseRight {
 
     static void staircase(int n) {
 
-        for(int i=n; i>0 ; i--){
+        for (int i = n; i > 0; i--) {
 
-             for(int j=1; j < i ; j++)
+            for (int j = 1; j < i; j++)
 
                 System.out.print(" ");
 
-             for(int k = 0 ; k < n-i+ 1 ; k++)
+            for (int k = 0; k < n - i + 1; k++)
 
-               System.out.print("#");
+                System.out.print("#");
 
             System.out.print("\n");
-         }
+        }
 
     }
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
         int n = scanner.nextInt();
