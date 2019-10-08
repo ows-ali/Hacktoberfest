33684e900a68fe2fc23ab768d5c4f10a73d16c70
Vicki <46823403+VickiLanger@users.noreply.github.com>
2019-10-08 16:11:51 -0400

    Create loops.py

---
+++ Hackerrank/loops.py
@@ -0,0 +1,8 @@
+https://www.hackerrank.com/challenges/python-loops/problem
+
+if __name__ == '__main__':
+    n = int(input()) #define variable
+
+# if input is less than N, print i squared
+for num in range(0, n): # define another variable "num" and look in range of 0 to n
+    print(num**2) 
