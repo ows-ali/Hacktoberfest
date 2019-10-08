a594d4ee7d5e8fe155b91bf7807cfb9fc35968eb
Vicki <46823403+VickiLanger@users.noreply.github.com>
2019-10-08 16:22:29 -0400

    Create print-function.py

---
+++ Hackerrank/print-function.py
@@ -0,0 +1,7 @@
+https://www.hackerrank.com/challenges/python-print/problem
+
+if __name__ == '__main__':
+    n = int(input()) # define variable
+
+    for num in range (1, n+1):
+        print(num, end='')
