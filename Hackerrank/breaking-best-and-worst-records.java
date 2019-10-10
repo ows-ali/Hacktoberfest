aac8d1e04b196dc2fcf339a164fc1ba5f978463b
neX- <hoanglongtran0698@gmail.com>
2019-10-10 23:43:57 +0700

    breaking-best-and-worst-records hackerrank problem

---
+++ Hackerrank/breaking-best-and-worst-records.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the breakingRecords function below.
+    static int[] breakingRecords(int[] scores) {
+        int currentMin = scores[0];
+        int currentMax = scores[0];
+        int[] result = new int[2];
+        for (int i = 1; i < scores.length; i++)
+        {
+            if (scores[i] > currentMax){
+                currentMax = scores[i];
+                result[0] += 1;
+            }
+            else if (scores[i] < currentMin){
+                currentMin = scores[i];
+                result[1] += 1;
+            }
+        }
+        return result;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] scores = new int[n];
+
+        String[] scoresItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int scoresItem = Integer.parseInt(scoresItems[i]);
+            scores[i] = scoresItem;
+        }
+
+        int[] result = breakingRecords(scores);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
