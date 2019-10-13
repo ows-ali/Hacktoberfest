79b3a289c9700d6b1b1ab012bec880a0082bec19
Khushi55 <43911479+Khushi55@users.noreply.github.com>
2019-10-13 21:59:38 +0530

    Create Loops.py

---
+++ Hackerrank/Loops.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,11):
+       
+        print(n,"x",i,"=",n*i) 
