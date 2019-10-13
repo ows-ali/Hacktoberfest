3eddb04b3c741aff9805aa1ec7f5065b7b7c5d63
gabbigum <48262816+gabbigum@users.noreply.github.com>
2019-10-13 12:17:57 +0100

    Create CurrencyFormat.java

---
+++ Hackerrank/CurrencyFormat.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class CurrencyFormater {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        Locale indiaLocale = new Locale("en", "IN");
+
+        NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat india = NumberFormat.getCurrencyInstance(indiaLocale);
+        NumberFormat china = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat france = NumberFormat.getInstance(Locale.FRANCE);
+
+
+        System.out.println("US: " + us.format(payment));
+        System.out.println("India: " + india.format(payment));
+        System.out.println("China: " + china.format(payment));
+        System.out.println("France: " + france.format(payment));
+    }
