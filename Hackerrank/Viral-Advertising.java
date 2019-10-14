3f8aebf0f94f74a95c2046601973c8161522b4fb
utkarsh963741 <51253240+utkarsh963741@users.noreply.github.com>
2019-10-15 00:51:30 +0530

    Create Viral-Advertising.java
    Solution to Viral Advertising hackerrank algorithm problem.
    https://www.hackerrank.com/challenges/strange-advertising/problem

---
+++ Hackerrank/Viral-Advertising.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the viralAdvertising function below.
+    static int viralAdvertising(int n) {
+  int likes=0;
+  int cummulative=0;
+  
+  for(int i=1;i<=n;i++){
+    if(i==1){
+      likes=2;
+    }
+    else{
+      likes=(likes*3)/2;
+    }
+      cummulative=cummulative+likes;
+  }
+   return cummulative;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int result = viralAdvertising(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
