1a8d240329d665537fcd778c7f571e7b15266551
JennaKoslowski <jenna.koslowski@yahoo.com>
2019-10-14 20:59:17 -0500

    add pointer exercise

---
+++ Hackerrank/POINTER.cpp
@@ -0,0 +1,20 @@
+//this solution includes adding the math.h library
+
+#include <stdio.h>
+#include <math.h>
+
+
+void update(int *a,int *b) {
+      *a = *a + *b; 
+      *b = abs(*a - *b - *b); //undo the adding b from previous. 
+}
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
