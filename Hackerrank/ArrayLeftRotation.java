ded5a43b11cf69f2b4b405cedcb810df07e065fd
AdiTOSH <56103824+Aditya-Singh-SSJ2@users.noreply.github.com>
2019-10-15 12:37:06 +0530

    Updated ArrayLeftRotate.java
    Hi, I just tried to improve the time complexity of the code, Hope you like it and all my efforts do not go in vain.
    I am trying for hactoberfest but cannot understand how and where i have to contribute to you, hope you’ll help me with that in someway.
    -Aditya Singh from NIT Rourkela 1st year

--- Hackerrank/ArrayLeftRotation.java
+++ Hackerrank/ArrayLeftRotation.java
@@ -1,7 +1,41 @@
 import java.util.Scanner;
 import java.util.*;
-public class ArrayLeftRotation {
+	class RotateArray{
+	void leftRotate(int arr[], int d, int n)
+	{
+		int i,j,k,temp;
+		int g_c_d = gcd(d,n);
+		for (i=0; i<g_c_d; i++) 
+		{
+			temp = arr[i];
+			j=i;
+			while(true)
+			{
+				k = j + d;
+				if (k>=n)
+					k = k-n;
+				if (k==i)
+					break;
+				arr[j] = arr[k];
+				j=k;
+			}
+			arr[j] = temp;
+		}
+	}
+		
+		
+	int gcd(int a, int b)
+	{
+		if (b==0)
+			return a;
+		else
+			return gcd(b, a%b);
+	}
+		
 	public static void main(String [] args) {
+		
+		RotateArray rotate = new RotateArray();
+		
 		Scanner keyboard=new Scanner (System.in);
 		int[] arr = new int[keyboard.nextInt()];
 		int rot = keyboard.nextInt();
@@ -10,17 +44,8 @@ public class ArrayLeftRotation {
 			arr[c]=keyboard.nextInt();
 		}
 		
-		int temp=0;
-		//left rotate array
-		for(int c=0; c<rot; c++) {
-			temp=arr[0];
-			//for each rotation shift
-			for(int i=0; i<arr.length-1;i++) {
-			arr[i]=arr[i+1];	
-			}
-			arr[arr.length-1]=temp;
-			
-		}
+		rotate.leftRotate(arr, rot, arr.length);
+		
 		for(int a : arr) {
 			System.out.print(a+"0");
 		}
