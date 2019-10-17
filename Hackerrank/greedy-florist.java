f5bde3ec8b68f8fe3b1c196f56c41e20ee76a24d
deepak kumar <deepak.mohan@infosys.com>
2019-10-18 01:09:46 +0530

    Adding solution to greeady-florist on hackerrank java by Deepak kumar

---
+++ Hackerrank/greedy-florist.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the getMinimumCost function below.
+    static int getMinimumCost(int k, int[] c) {
+
+        if (c.length <= k) 
+            return Arrays.stream(c).sum();
+        else {
+            int pricePaid = 0;
+            Arrays.sort(c);
+            for (int temp=c.length-1; temp >= 0; temp--) {
+                pricePaid += ((c.length-1-temp)/k+1)*c[temp];
+                // System.out.print(pricePaid+"--");
+            }
+            return pricePaid;
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] c = new int[n];
+
+        String[] cItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int cItem = Integer.parseInt(cItems[i]);
+            c[i] = cItem;
+        }
+
+        int minimumCost = getMinimumCost(k, c);
+
+        bufferedWriter.write(String.valueOf(minimumCost));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
