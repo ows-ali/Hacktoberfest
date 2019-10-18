8ac1e2dccd037f73a7cbbbb63c89bf672cd1f7c9
Morgan McNamara <gny.morganmc@gmail.com>
2019-10-18 13:51:27 -0500

    Adding Hackerrank solution to Java Stdin Stdout Problem

---
+++ Hackerrank/Java_Stdin_and_Stdout_1.java
@@ -0,0 +1,38 @@
+
+/*
+
+* https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem
+* run this code in the application to check your answer and then submit the solution!
+*/
+
+
+
+import java.util.*;
+
+
+
+public class Solution {
+
+
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+
+        int b = scan.nextInt();
+
+        int c = scan.nextInt();
+
+        scan.close();
+
+        System.out.println(a);
+
+        System.out.println(b);
+
+        System.out.println(c);
+
+    }
+
+}
