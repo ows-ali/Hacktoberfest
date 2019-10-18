25d4c056f9afb20e3fb6a342f9e9fe4d6b222c93
Brandon Ubben <brandon@ubben.co>
2019-10-18 12:08:59 -0700

    Added solution for ctci-array-left-rotation on Hackerrank

---
+++ Hackerrank/ctci-array-left-rotation.js
@@ -0,0 +1,64 @@
+// https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem
+// Passed all test cases, makes use of ES6
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the rotLeft function below.
+function rotLeft(a, d) {
+    // Solution
+    const aCopy = a.slice();
+    const newArr = [];
+
+    let i = 0;
+    while (i < d) {
+        newArr.push(aCopy.shift());
+        i++;
+    }
+
+    newArr.unshift(...aCopy);
+
+    return newArr;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const nd = readLine().split(' ');
+
+    const n = parseInt(nd[0], 10);
+
+    const d = parseInt(nd[1], 10);
+
+    const a = readLine().split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = rotLeft(a, d);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
+
