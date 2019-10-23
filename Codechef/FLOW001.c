8042e2bdae656a84f3e962afed1f1e9023455210
Rakshit Naidu <rakshit.naidu07@gmail.com>
2019-10-24 00:56:34 +0530

    Create FLOW001.c

---
+++ Codechef/FLOW001.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+
+int main() {
+	// Read the number of test cases.
+	int T;
+	scanf("%d", &T);
+	while (T--) {
+		// Read the input a, b
+		int a, b;
+		scanf("%d %d", &a, &b);
+
+		// Compute the ans.
+		int ans = a + b;
+		printf("%d\n", ans);
+	}
+
+	return 0;
+}
