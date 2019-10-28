af2a787c3d530ae0e48083d767f6d8f953b7543b
Sai Eashwar K S <saieashwar.ks@gmail.com>
2019-10-28 11:00:46 +0530

    Minimum_Absolute_Difference_in_an_Array using C++

---
+++ Hackerrank/Minimum_Absolute_Difference_in_an_Array.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the minimumAbsoluteDifference function below.
+int minimumAbsoluteDifference(vector<int> arr) {
+    int n = arr.size();
+    sort(arr.begin(), arr.end());
+    long int mini = abs(arr[0] - arr[1]);
+    if (mini == 0)
+        return 0;
+        for (int i = 2; i < n; ++i)
+        {
+            if((abs(arr[i] - arr[i-1]))<mini)
+                mini = abs(arr[i] - arr[i-1]);
+        }
+    return mini;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int result = minimumAbsoluteDifference(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
+
