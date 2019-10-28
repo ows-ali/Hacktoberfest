ba569278524f82eb2b6f5e7d2eea7a2cfcfcd7e9
dhruv017 <57063142+dhruv017@users.noreply.github.com>
2019-10-28 13:46:05 +0530

    Update Staircase.py
    changed output method using return function

--- Hackerrank/Staircase.py
+++ Hackerrank/Staircase.py
@@ -6,9 +6,9 @@ import random
 import re
 import sys
 
-# Complete the staircase function below.
 def staircase(n):
     for row in range(1,n+1):
         return ((n - row) * ' ' + row * '#')
 n = int(input())
 print(staircase(n))
+
