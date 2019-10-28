c166533709611290ef16701f6fcac45577bc06ba
abdulmk787 <55476136+abdulmk787@users.noreply.github.com>
2019-10-28 12:45:10 +0530

    A more efficient code

--- Udemy/is-prime.py
+++ Udemy/is-prime.py
@@ -1,13 +1,15 @@
 def is_prime(number):
-    if(number < 2):
+    if (number < 2):
         return False
-        
-    #check if number is divisible by 2 to number - 1
-    for divisor in range(2, number):
+
+    # check if number is divisible by 2 to number/2 +1 so number of iterations are reduced
+    # the largest number that can divide the number is its half
+    for divisor in range(2, number // 2 + 1):
         if number % divisor == 0:
             return False
-            
+
     return True
-    
-print(is_prime(15))
+
+
+print(is_prime(102))
 print(is_prime(10))
