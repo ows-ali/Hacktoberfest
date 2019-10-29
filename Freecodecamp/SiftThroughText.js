90d2162ac786fed70ea9225a81325b10cc999a05
sheezz-code <57119689+sheezz-code@users.noreply.github.com>
2019-10-28 21:21:37 -0400

    Create SiftThroughText.js

---
+++ Freecodecamp/SiftThroughText.js
@@ -0,0 +1,16 @@
+// Setup
+var testString = "Ada Lovelace and Charles Babbage designed the first computer and the software that would have run on it.";
+
+// Example
+var expressionToGetSoftware = /software/gi;
+var softwareCount = testString.match(expressionToGetSoftware).length;
+  
+
+// Only change code below this line.
+
+var expression = /and/gi;  // Change this Line
+
+// Only change code above this line
+
+// This code counts the matches of expression in testString
+var andCount = testString.match(expression).length;
