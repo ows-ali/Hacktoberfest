ff8e250c0e843eadf80596ac8497ce03ce7de3ae
aaggarwal1 <aditi_aggarwal@intuit.com>
2020-05-22 09:41:01 +0530

    Add vector-sort

---
+++ Hackerrank/vector-sort.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    vector<int> a;
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++)
+        {
+        int x;
+        cin>>x;
+        a.push_back(x);
+    }
+    sort(a.begin(), a.end());
+    for(int i=0; i<n; i++)
+        cout<<a[i]<<" ";
+    return 0;
+}
+
