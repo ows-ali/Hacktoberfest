08de5b8336c91efc3c9c698ed0f7160f12ff05ee
Ysoeun <sycaroline529@gmail.com>
2020-10-04 18:00:31 +0900

    https://www.hackerrank.com/challenges/compare-the-triplets/problem Added this problem in hackerrank folder

---
+++ Hackerrank/Compare_the_Triplets.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+    alice = 0
+    bob = 0
+    for i in range(3):
+        if a[i] > b[i]:
+            alice += 1
+        elif b[i] > a[i]:
+            bob += 1
+    li =[alice,bob]
+    return li
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
