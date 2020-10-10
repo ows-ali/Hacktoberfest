bf299f6ada540f42d0b7cb33e4f4fb98ed4837c7
Infinityz <40041126+Infinityz@users.noreply.github.com>
2020-10-10 15:21:40 -0400

    Electronics_Shop.java
    issue refrence (#1)
    
    https://www.hackerrank.com/challenges/electronics-shop/problem
    
    - Added Electronics_Shop.java
    - Added profile to README.md

---
+++ Hackerrank/Electronics_Shop.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+//Hactoberfest 2020
+
+public class Electronics_Shop {
+
+
+    static int getMoneySpent(int[] keyboards, int[] drives, int b) {
+
+        int current = -1;
+        for (int i = 0; i < keyboards.length; i++){
+            for (int j = 0; j < drives.length; j++){
+                int combination = keyboards[i] + drives[j];
+                if (combination > current && combination <= b)
+                    current = combination;
+            }
+        }
+
+        return current;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] bnm = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])*");
+
+        int b = Integer.parseInt(bnm[0]);
+
+        int n = Integer.parseInt(bnm[1]);
+
+        int m = Integer.parseInt(bnm[2]);
+
+        int[] keyboards = new int[n];
+
+        String[] keyboardsItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])*");
+
+        for (int keyboardsItr = 0; keyboardsItr < n; keyboardsItr++) {
+            int keyboardsItem = Integer.parseInt(keyboardsItems[keyboardsItr]);
+            keyboards[keyboardsItr] = keyboardsItem;
+        }
+
+        int[] drives = new int[m];
+
+        String[] drivesItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])*");
+
+        for (int drivesItr = 0; drivesItr < m; drivesItr++) {
+            int drivesItem = Integer.parseInt(drivesItems[drivesItr]);
+            drives[drivesItr] = drivesItem;
+        }
+
+        int moneySpent = getMoneySpent(keyboards, drives, b);
+
+        bufferedWriter.write(String.valueOf(moneySpent));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
