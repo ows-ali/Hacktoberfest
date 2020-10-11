c07a0f4a736cd41f531bc5c8d941fd6a3f27018d
Luís Tovar <1181585@isep.ipp.pt>
2020-10-11 10:55:51 +0100

    Solved diagonal-difference problem in Java

---
+++ Hackerrank/diagonal-difference.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        int sum1 = 0;
+        for (int i=0; i<arr.size(); i++)
+            sum1 += arr.get(i).get(i);
+
+        int sum2 = 0;
+        for (int i=0; i<arr.size(); i++)
+            sum2 += arr.get(i).get(arr.size()-1-i);
+
+        return Math.abs(sum1-sum2);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                arr.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = Result.diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
