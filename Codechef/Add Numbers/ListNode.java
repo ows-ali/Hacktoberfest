e3a57250a940df30df93ca56164a618cd558d58e
Shambhu Gohel <gohelshambhu95@yahoo.com>
2020-10-18 12:31:25 +0530

    Added AddTwoNumber Problem's Solution

---
+++ Codechef/Add Numbers/ListNode.java
@@ -0,0 +1,7 @@
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(){}
+    ListNode(int val) {this.val=val;}
+    ListNode(int val, ListNode next){this.val=val; this.next=next;}
+}
