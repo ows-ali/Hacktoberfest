9eb35af777683ec8e6980be7ac21657cda7bde68
Justin Giffard <justin.giffard@absa.africa>
2020-10-20 13:59:18 +0200

    https://www.hackerrank.com/challenges/grading/problem

---
+++ Hackerrank/grading.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+import os
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    # Write your code here
+    round5 = lambda x: round(x/5)*5
+    return map(lambda x: round5(x) if x>=38 and round5(x)>x else x, grades)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
