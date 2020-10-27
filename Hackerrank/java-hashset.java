b1d731568b33d89d5955364a9f93779eb8841886
akshaychaudhari <akshaychaudhari12345@gmail.com>
2020-10-28 00:11:03 +0530

    added 3 Hackerrank solutions

---
+++ Hackerrank/java-hashset.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+ public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String [] pair_left = new String[t];
+        String [] pair_right = new String[t];
+        
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+
+        //Write your code here
+        Set<String> set = new HashSet<String>();
+        for (int i = 0; i < t; i++) {
+            set.add(pair_left[i] + "_" + pair_right[i]);
+            System.out.println(set.size());
+
+        }
+   }
+}
