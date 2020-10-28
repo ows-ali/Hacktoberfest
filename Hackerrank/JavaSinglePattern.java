22bd37e8b583b152bbc8a4508b8e085c28f495fc
Vince-V <vincevillas2@gmail.com>
2020-10-27 23:33:41 -0500

    Added details about singleton,and double-checked locking

--- Hackerrank/JavaSinglePattern.java
+++ Hackerrank/JavaSinglePattern.java
@@ -1,20 +1,28 @@
 
-
 public class JavaSinglePattern {
-	
 	public static void main(String[] args) {
+		String strCompare = "Hello I am a singleton! Let me say hello world to you";
+		
 		Singleton singleton1 = Singleton.getSingleInstance();
-		System.out.println(singleton1.str);
+		System.out.println(singleton1.str +" and this is my hashcode: "+ singleton1.hashCode());
 		
 		Singleton singleton2 = Singleton.getSingleInstance();
-		System.out.println(singleton2.str);
+		System.out.println(singleton2.str +" and this is my hashcode: "+ singleton2.hashCode());
+		
+		if (singleton2.str.equals(strCompare)){
+			System.out.println(" The value of my local variable is equal to the the singleton");
+			if (singleton2.str == strCompare){
+				System.out.println(" But it is not the same object, hashcode : " + strCompare.hashCode());
+			}
+		}
+		
 	}
 
 }
 
 class Singleton {
 	
-	private static Singleton singleton;
+	private static volatile Singleton singleton; //volatile helps to prevent cache incoherence issues
 	
 	public String str = "Hello I am a singleton! Let me say hello world to you";
 	
@@ -24,8 +32,14 @@ class Singleton {
 	
 	public static Singleton getSingleInstance() {
 		
+		//Added double check locking
 		if (null == singleton) {
-			singleton = new Singleton();
+			synchronized (Singleton.class) {
+				if (null == singleton){
+					 singleton = new Singleton();
+				}
+			}
+			 
 		}
 		
 		return singleton;
