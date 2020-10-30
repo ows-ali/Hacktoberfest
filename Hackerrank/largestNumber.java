7cd023b5e46051c536f94201e396191c242da53d
Ashen Kavinda <49136460+Ashenk97@users.noreply.github.com>
2020-10-30 18:58:17 +0530

    Finding the largest number from an array using Java

---
+++ Hackerrank/largestNumber.java
@@ -0,0 +1,23 @@
+public class LargestInArrayExample{  
+public static int getLargest(int[] a, int total){  
+int temp;  
+for (int i = 0; i < total; i++)   
+        {  
+            for (int j = i + 1; j < total; j++)   
+            {  
+                if (a[i] > a[j])   
+                {  
+                    temp = a[i];  
+                    a[i] = a[j];  
+                    a[j] = temp;  
+                }  
+            }  
+        }  
+       return a[total-1];  
+}  
+public static void main(String args[]){  
+int a[]={1,2,5,6,3,2};  
+int b[]={44,66,99,77,33,22,55};  
+System.out.println("Largest: "+getLargest(a,6));  
+System.out.println("Largest: "+getLargest(b,7));  
+}}  
