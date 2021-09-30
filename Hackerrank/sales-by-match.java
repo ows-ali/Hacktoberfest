b613f9cd790f3ddf61859be950f4fd49a40edd0d
Sudhanshu <77659465+sudhanshu2300@users.noreply.github.com>
2021-10-01 01:10:26 +0530

    Create sales-by-match.java
    input -
    9
    10 20 20 10 10 30 50 10 20
    output-
    3

---
+++ Hackerrank/sales-by-match.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+     HashMap<Integer ,Integer> hm = new HashMap();
+     for(int i = 0 ;i<ar.size();i++){
+         int k = ar.get(i);
+         if(hm.containsKey(k)){
+            int old = hm.get(k);
+            old++;
+            hm.put(k , old);
+         }else{
+             hm.put(k ,1);
+         }
+     }
+     int count =0;
+     for(Integer k : hm.keySet()){
+         int div = hm.get(k)/2;
+         count +=div;
+     }
+     return count;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
