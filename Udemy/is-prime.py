fee14cb366242d09f518a5f9d0173bda22d4583e
arizzaa13 <78647475+arizzaa13@users.noreply.github.com>
2021-10-23 19:55:37 +0530

    Update is-prime.py

--- Udemy/is-prime.py
+++ Udemy/is-prime.py
@@ -2,7 +2,7 @@ def is_prime(number):
     if(number < 2):
         return False
         
-    #check if number is divisible by 2 to number - 1
+    #check if number is divisible by 2 to - 1
     for divisor in range(2, number):
         if number % divisor == 0:
             return False
