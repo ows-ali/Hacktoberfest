9ecb7d3dcc454d5e12401875e2d14e58248cc57b
Imran Biswas <imranbiswas2209@gmail.com>
2022-09-30 01:46:38 +0530

    Add print all element in a linked list solution.

---
+++ Hackerrank/printLinkedList.cpp
@@ -0,0 +1,91 @@
+// Problem Name - Print all elements of a linked list.
+// Problem Link - https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list/problem?isFullScreen=true
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class SinglyLinkedListNode
+{
+public:
+    int data;
+    SinglyLinkedListNode *next;
+
+    SinglyLinkedListNode(int node_data)
+    {
+        this->data = node_data;
+        this->next = nullptr;
+    }
+};
+
+class SinglyLinkedList
+{
+public:
+    SinglyLinkedListNode *head;
+    SinglyLinkedListNode *tail;
+
+    SinglyLinkedList()
+    {
+        this->head = nullptr;
+        this->tail = nullptr;
+    }
+
+    void insert_node(int node_data)
+    {
+        SinglyLinkedListNode *node = new SinglyLinkedListNode(node_data);
+
+        if (!this->head)
+        {
+            this->head = node;
+        }
+        else
+        {
+            this->tail->next = node;
+        }
+
+        this->tail = node;
+    }
+};
+
+void free_singly_linked_list(SinglyLinkedListNode *node)
+{
+    while (node)
+    {
+        SinglyLinkedListNode *temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+void printLinkedList(SinglyLinkedListNode *head)
+{
+    SinglyLinkedListNode *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->data << endl;
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    SinglyLinkedList *llist = new SinglyLinkedList();
+
+    int llist_count;
+    cin >> llist_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int i = 0; i < llist_count; i++)
+    {
+        int llist_item;
+        cin >> llist_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        llist->insert_node(llist_item);
+    }
+
+    printLinkedList(llist->head);
+
+    return 0;
+}
