f715c4534ae4e1691535f6f35fbe89352a770196
Agrim <agrimsharma1714@gmail.com>
2022-10-03 22:22:18 +0530

    added classes_and_objects.cpp file

---
+++ Hackerrank/classes_and_objects.cpp
@@ -0,0 +1,52 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+// Write your Student class here
+class Student{
+    public:
+  int scores[5];
+  void input(){
+      for(int i = 0; i < 5; i++){
+          cin >> scores[i];
+      }
+  }  
+  int calculateTotalScore(){
+      int sum = 0;
+      for(int i = 0; i < 5; i++){
+          sum += scores[i];
+      }
+      return sum;
+  }
+};
+
+int main() {
+    int n; // number of students
+    cin >> n;
+    Student *s = new Student[n]; // an array of n students
+    
+    for(int i = 0; i < n; i++){
+        s[i].input();
+    }
+
+    // calculate kristen's score
+    int kristen_score = s[0].calculateTotalScore();
+
+    // determine how many students scored higher than kristen
+    int count = 0; 
+    for(int i = 1; i < n; i++){
+        int total = s[i].calculateTotalScore();
+        if(total > kristen_score){
+            count++;
+        }
+    }
+
+    // print result
+    cout << count;
+    
+    return 0;
+}
