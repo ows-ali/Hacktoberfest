58f40c8f6961e2fe574aa7c3b181f861265d3c77
Anuj Patel <89893953+anujpatel581@users.noreply.github.com>
2022-10-09 00:48:01 +0530

    Podium_Finish.java
    Podium_Finish.java  from code chef

---
+++ Codechef/Podium_Finish.java
@@ -0,0 +1,22 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner scan = new Scanner(System.in);
+	int t = scan.nextInt();
+	while(t>0){
+	    int a = scan.nextInt();
+	    int b = scan.nextInt();
+	    int res = a+b;
+	    System.out.println(res);
+	    t--;
+	}
+	}
+}
