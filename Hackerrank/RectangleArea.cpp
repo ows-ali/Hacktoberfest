6c29acd8fd49cd189c2647d00fda9fe158bc602d
siddd26 <105415987+siddd26@users.noreply.github.com>
2022-10-11 17:52:36 +0530

    Created RectangleArea.cpp
    This is a C++ program to calculate the area of a rectangle using Classe and Functions

---
+++ Hackerrank/RectangleArea.cpp
@@ -0,0 +1,24 @@
+
+/*
+ * Create classes Rectangle and RectangleArea
+ */
+class Rectangle{
+    public:
+    int l,b;
+    void display(){
+        cout<<l<<"\t"<<b<<endl;
+    }
+};
+
+class RectangleArea:public Rectangle{
+    public:
+    void read_input(){
+        cin>>l>>b;
+        
+        
+    }
+    void display(){
+        cout<<l*b<<endl;
+    }
+};
+
