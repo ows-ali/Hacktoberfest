ca3a3eafcfe6f61e2fdeb69477fcffa66e7fd84d
Abhijeet Vishwakarma <vishwakarma2003@hotmail.com>
2022-10-19 22:57:48 +0530

    https://leetcode.com/problems/missing-number/

--- Leetcode/missing-number.java
+++ Leetcode/missing-number.java
@@ -5,27 +5,28 @@ public class power_set {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter a String  for Power sets");
-        String str = sc.next();
-        System.out.println("Power Set of " + str);
-        set_power(str);
-
+        int arrp[] = { 1, 2, 3, 5 };
+        int missing_num = missingNumber(arr);
+        System.out.println("Missing number --> " + missing_num);
         sc.close();
     }
 
-    // We have solved it in O(2^n)
-    public static void set_power(String str) {
-
-        for (int i = 0; i < (Math.pow(2, str.length())); i++) {
-
-            for (int j = 0; j < str.length(); j++) {
-                // choosing the allowed characters using set bit positions
-                if ((i & (1 << j)) != 0) {
-                    System.out.print(str.charAt(j) + "");
-                }
-            }
-            System.out.println();
+    public int missingNumber(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            // Calcualting XOR of all the elements in the array
+            // res = arr[0] ^ arr[1] ^ .....
+            res ^= nums[i];
         }
 
-    }
+        for (int i = 0; i <= nums.length; i++) {
+            // XOR of all the elements in the arrry with total range elements
+            // USing the XOR properties repeating elements cancel each other
+            // Atlast Missing ( non repeating ) element is left behind in the result
+            // variable
+            res = res ^ i;
+        }
+        return res;
 
+    }
 }
