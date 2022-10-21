e2ad1e112b1a8f38d66cca84040e6e944d640dff
jeevan-spec <73019593+jeevan-spec@users.noreply.github.com>
2022-10-21 16:35:10 +0530

    Create grade-analyser.py
    This program tells your grade.
    For Hacktoberfest :)

---
+++ grade-analyser.py
@@ -0,0 +1,7 @@
+#Program to tell your grade.
+m= float(input("ENTER YOUR PERCENTAGE: "))
+if m>=85: print("YOUR GRADE IS A")
+elif m>=70: print("YOUR GRADE IS B")
+elif m>=60: print("YOUR GRADE IS C")
+elif m>=45: print("YOUR GRADE IS D")
+elif m<45: print("YOUR GRADE IS E")
